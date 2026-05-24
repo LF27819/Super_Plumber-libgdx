f8a79b70fce4e398de3b964e111a49c4528817b3
LF27819 <luciafeliperuiz@gmail.com>
2026-05-24 11:06:07 +0200

    Añadir movimiento y salto del jugador

--- core/src/main/java/com/svalero/Super_Plumber/screen/GameScreen.java
+++ core/src/main/java/com/svalero/Super_Plumber/screen/GameScreen.java
@@ -4,7 +4,7 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.svalero.Super_Plumber.Super_Plumber;
 
@@ -13,7 +13,18 @@ public class GameScreen implements Screen {
     private final Super_Plumber game;
 
     private SpriteBatch batch;
-    private BitmapFont font;
+    private Texture marioTexture;
+
+    private float marioX;
+    private float marioY;
+
+    private float speed;
+    private float verticalSpeed;
+    private float gravity;
+    private float jumpForce;
+    private float groundY;
+
+    private boolean isOnGround;
 
     public GameScreen(Super_Plumber game) {
         this.game = game;
@@ -23,8 +34,20 @@ public class GameScreen implements Screen {
     public void show() {
         batch = new SpriteBatch();
 
-        font = new BitmapFont();
-        font.getData().setScale(2);
+        marioTexture = new Texture(
+            Gdx.files.internal("assets/sprites/player/mario-dcha.png")
+        );
+
+        marioX = 100;
+        groundY = 120;
+        marioY = groundY;
+
+        speed = 250;
+        verticalSpeed = 0;
+        gravity = -900;
+        jumpForce = 450;
+
+        isOnGround = true;
     }
 
     @Override
@@ -34,13 +57,40 @@ public class GameScreen implements Screen {
             game.setScreen(new MainMenuScreen(game));
         }
 
+        if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
+            marioX += speed * delta;
+        }
+
+        if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
+            marioX -= speed * delta;
+        }
+
+        if (Gdx.input.isKeyJustPressed(Input.Keys.SPACE) && isOnGround) {
+            verticalSpeed = jumpForce;
+            isOnGround = false;
+        }
+
+        verticalSpeed += gravity * delta;
+        marioY += verticalSpeed * delta;
+
+        if (marioY <= groundY) {
+            marioY = groundY;
+            verticalSpeed = 0;
+            isOnGround = true;
+        }
+
         Gdx.gl.glClearColor(0.4f, 0.7f, 1f, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
         batch.begin();
 
-        font.draw(batch, "GAME SCREEN", 350, 400);
-        font.draw(batch, "ESC para volver al menu", 280, 330);
+        batch.draw(
+            marioTexture,
+            marioX,
+            marioY,
+            64,
+            64
+        );
 
         batch.end();
     }
@@ -48,15 +98,11 @@ public class GameScreen implements Screen {
     @Override public void resize(int width, int height) {}
     @Override public void pause() {}
     @Override public void resume() {}
-
-    @Override
-    public void hide() {
-        dispose();
-    }
+    @Override public void hide() {}
 
     @Override
     public void dispose() {
         if (batch != null) batch.dispose();
-        if (font != null) font.dispose();
+        if (marioTexture != null) marioTexture.dispose();
     }
 }
