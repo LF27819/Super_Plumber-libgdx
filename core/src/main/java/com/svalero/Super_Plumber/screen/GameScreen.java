6ab1d14c0a6abfb67ec343b59b5d452e12a8f09f
LF27819 <luciafeliperuiz@gmail.com>
2026-05-24 16:27:02 +0200

    Mario + bloques suelo y plataforma reales

--- core/src/main/java/com/svalero/Super_Plumber/screen/GameScreen.java
+++ core/src/main/java/com/svalero/Super_Plumber/screen/GameScreen.java
@@ -5,7 +5,6 @@ import com.badlogic.gdx.Input;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Rectangle;
 import com.svalero.Super_Plumber.Super_Plumber;
@@ -15,28 +14,27 @@ public class GameScreen implements Screen {
     private final Super_Plumber game;
 
     private SpriteBatch batch;
-    private ShapeRenderer shapeRenderer;
-    private Texture marioTexture;
 
-    // POSICION Y TAMAÑO
+    private Texture marioTexture;
+    private Texture groundBlockTexture;
+
     private float marioX;
     private float marioY;
     private float marioWidth;
     private float marioHeight;
 
-    // MOVIMIENTO Y FISICAS
     private float speed;
     private float verticalSpeed;
     private float gravity;
     private float jumpForce;
 
-    // ESTADO
     private boolean isOnGround;
 
-    // COLISIONES
     private Rectangle marioBounds;
     private Rectangle ground;
     private Rectangle platform;
+
+    private final int blockSize = 48;
 
     public GameScreen(Super_Plumber game) {
         this.game = game;
@@ -44,198 +42,115 @@ public class GameScreen implements Screen {
 
     @Override
     public void show() {
-
         batch = new SpriteBatch();
 
-        shapeRenderer = new ShapeRenderer();
+        marioTexture = new Texture(Gdx.files.internal("assets/sprites/player/mario-dcha.png"));
+        groundBlockTexture = new Texture(Gdx.files.internal("assets/sprites/tiles/bloque-ladrillo.png"));
 
-        marioTexture = new Texture(
-            Gdx.files.internal("assets/sprites/player/mario-dcha.png")
-        );
-
-        // TAMAÑO MARIO
-        marioWidth = 48;
+        marioWidth = 64;
         marioHeight = 64;
 
-        // POSICION INICIAL
         marioX = 100;
         marioY = 150;
 
-        // MOVIMIENTO
         speed = 250;
-
-        // FISICAS
         verticalSpeed = 0;
         gravity = -900;
-        jumpForce = 480;
+        jumpForce = 520;
 
         isOnGround = false;
 
-        // RECTANGULO MARIO
-        marioBounds = new Rectangle(
-            marioX,
-            marioY,
-            marioWidth,
-            marioHeight
-        );
+        marioBounds = new Rectangle(marioX, marioY, marioWidth, marioHeight);
 
-        // SUELO
-        ground = new Rectangle(
-            0,
-            80,
-            Gdx.graphics.getWidth(),
-            40
-        );
-
-        // PLATAFORMA
-        platform = new Rectangle(
-            280,
-            190,
-            260,
-            30
-        );
+        ground = new Rectangle(0, 0, Gdx.graphics.getWidth(), blockSize);
+        platform = new Rectangle(260, 145, blockSize * 5, blockSize);
     }
 
     @Override
     public void render(float delta) {
-
-        // VOLVER AL MENU
         if (Gdx.input.isKeyJustPressed(Input.Keys.ESCAPE)) {
             game.setScreen(new MainMenuScreen(game));
         }
 
         handleInput(delta);
-
         applyGravity(delta);
-
         checkCollisions();
 
-        // LIMPIAR PANTALLA
-        Gdx.gl.glClearColor(0.4f, 0.7f, 1f, 1);
-
+        Gdx.gl.glClearColor(0.35f, 0.65f, 1f, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
-        // DIBUJAR SUELO Y PLATAFORMAS
-        shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
-
-        shapeRenderer.rect(
-            ground.x,
-            ground.y,
-            ground.width,
-            ground.height
-        );
-
-        shapeRenderer.rect(
-            platform.x,
-            platform.y,
-            platform.width,
-            platform.height
-        );
-
-        shapeRenderer.end();
-
-        // DIBUJAR MARIO
         batch.begin();
 
-        batch.draw(
-            marioTexture,
-            marioX,
-            marioY,
-            marioWidth,
-            marioHeight
-        );
+        drawGround();
+        drawPlatform();
+
+        batch.draw(marioTexture, marioX, marioY, marioWidth, marioHeight);
 
         batch.end();
     }
 
-    private void handleInput(float delta) {
+    private void drawGround() {
+        for (int x = 0; x < Gdx.graphics.getWidth(); x += blockSize) {
+            batch.draw(groundBlockTexture, x, ground.y, blockSize, blockSize);
+        }
+    }
 
-        // DERECHA
+    private void drawPlatform() {
+        for (int x = (int) platform.x; x < platform.x + platform.width; x += blockSize) {
+            batch.draw(groundBlockTexture, x, platform.y, blockSize, blockSize);
+        }
+    }
+
+    private void handleInput(float delta) {
         if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
             marioX += speed * delta;
         }
 
-        // IZQUIERDA
         if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
             marioX -= speed * delta;
         }
 
-        // SALTO
         if (Gdx.input.isKeyJustPressed(Input.Keys.SPACE) && isOnGround) {
-
             verticalSpeed = jumpForce;
-
             isOnGround = false;
         }
     }
 
     private void applyGravity(float delta) {
-
         verticalSpeed += gravity * delta;
-
         marioY += verticalSpeed * delta;
 
         marioBounds.setPosition(marioX, marioY);
-
         isOnGround = false;
     }
 
     private void checkCollisions() {
-
         checkPlatformCollision(ground);
-
         checkPlatformCollision(platform);
     }
 
     private void checkPlatformCollision(Rectangle rectangle) {
-
         if (marioBounds.overlaps(rectangle) && verticalSpeed <= 0) {
-
-            float previousBottom =
-                marioY - verticalSpeed * Gdx.graphics.getDeltaTime();
+            float previousBottom = marioY - verticalSpeed * Gdx.graphics.getDeltaTime();
 
             if (previousBottom >= rectangle.y + rectangle.height - 5) {
-
                 marioY = rectangle.y + rectangle.height;
-
                 verticalSpeed = 0;
-
                 isOnGround = true;
-
                 marioBounds.setPosition(marioX, marioY);
             }
         }
     }
 
-    @Override
-    public void resize(int width, int height) {
-    }
-
-    @Override
-    public void pause() {
-    }
-
-    @Override
-    public void resume() {
-    }
-
-    @Override
-    public void hide() {
-    }
+    @Override public void resize(int width, int height) {}
+    @Override public void pause() {}
+    @Override public void resume() {}
+    @Override public void hide() {}
 
     @Override
     public void dispose() {
-
-        if (batch != null) {
-            batch.dispose();
-        }
-
-        if (shapeRenderer != null) {
-            shapeRenderer.dispose();
-        }
-
-        if (marioTexture != null) {
-            marioTexture.dispose();
-        }
+        if (batch != null) batch.dispose();
+        if (marioTexture != null) marioTexture.dispose();
+        if (groundBlockTexture != null) groundBlockTexture.dispose();
     }
 }
