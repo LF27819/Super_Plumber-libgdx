1b388858e35e2615a8ab9d8cc92ed18ca1fa3c4a
LF27819 <luciafeliperuiz@gmail.com>
2026-05-27 17:10:52 +0200

    HUD + pantalla game over

--- core/src/main/java/com/svalero/Super_Plumber/screen/GameOverScreen.java
+++ core/src/main/java/com/svalero/Super_Plumber/screen/GameOverScreen.java
@@ -6,21 +6,23 @@ import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.GlyphLayout;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.svalero.Super_Plumber.Super_Plumber;
 import com.svalero.Super_Plumber.manager.FontManager;
 
-//Sin usar aun
 public class GameOverScreen implements Screen {
 
     private final Super_Plumber game;
 
-    private SpriteBatch        batch;
+    private SpriteBatch batch;
     private OrthographicCamera camera;
-    private BitmapFont         font;
-    private GlyphLayout        layout;
+    private BitmapFont titleFont;
+    private BitmapFont textFont;
+    private GlyphLayout textLayout;
+    private Texture backgroundImage;
 
     public GameOverScreen(Super_Plumber game) {
         this.game = game;
@@ -28,43 +30,69 @@ public class GameOverScreen implements Screen {
 
     @Override
     public void show() {
-        batch  = new SpriteBatch();
+        batch = new SpriteBatch();
+
         camera = new OrthographicCamera();
         camera.setToOrtho(false, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
-        font   = FontManager.createMarioFont(20, Color.RED);
-        layout = new GlyphLayout();
+
+        titleFont = FontManager.createMarioFont(18, Color.RED);
+        textFont = FontManager.createMarioFont(12, Color.WHITE);
+        textLayout = new GlyphLayout();
+
+        backgroundImage = new Texture(Gdx.files.internal("assets/sprites/ui/game_over_background.png"));
     }
 
     @Override
     public void render(float delta) {
-        if (Gdx.input.isKeyJustPressed(Input.Keys.ENTER) ||
-            Gdx.input.isKeyJustPressed(Input.Keys.ESCAPE)) {
-            game.setScreen(new MainMenuScreen(game));
-        }
+        handleInput();
+        clearScreen();
 
-        Gdx.gl.glClearColor(0f, 0f, 0f, 1f);
-        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-
-        float sw = Gdx.graphics.getWidth();
-        float sh = Gdx.graphics.getHeight();
+        float screenWidth = Gdx.graphics.getWidth();
+        float screenHeight = Gdx.graphics.getHeight();
 
         batch.setProjectionMatrix(camera.combined);
         batch.begin();
 
-        FontManager.drawCentered(batch, font, layout, "GAME OVER", sw, sh / 2f + 60, 3f);
-        FontManager.drawCentered(batch, font, layout, "Pulsa ENTER para volver al menu", sw, sh / 2f, 1.2f);
+        drawBackground(screenWidth, screenHeight);
+
+        FontManager.drawCentered(batch, textFont, textLayout, "Pulsa ENTER para volver", screenWidth, screenHeight / 2f - 120, 1f);
+        FontManager.drawCentered(batch, textFont, textLayout, "al menu principal", screenWidth, screenHeight / 2f - 160, 1f);
 
         batch.end();
     }
 
-    @Override public void resize(int w, int h) { camera.setToOrtho(false, w, h); }
-    @Override public void pause()  {}
+    private void handleInput() {
+        if (Gdx.input.isKeyJustPressed(Input.Keys.ENTER)
+            || Gdx.input.isKeyJustPressed(Input.Keys.ESCAPE)) {
+            game.setScreen(new MainMenuScreen(game));
+        }
+    }
+
+    private void clearScreen() {
+        Gdx.gl.glClearColor(0f, 0f, 0f, 1f);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+    }
+
+    private void drawBackground(float screenWidth, float screenHeight) {
+        if (backgroundImage != null) {
+            batch.draw(backgroundImage, 0, 0, screenWidth, screenHeight);
+        }
+    }
+
+    @Override
+    public void resize(int screenWidth, int screenHeight) {
+        camera.setToOrtho(false, screenWidth, screenHeight);
+    }
+
+    @Override public void pause() {}
     @Override public void resume() {}
-    @Override public void hide()   {}
+    @Override public void hide() {}
 
     @Override
     public void dispose() {
         if (batch != null) batch.dispose();
-        if (font  != null) font.dispose();
+        if (titleFont != null) titleFont.dispose();
+        if (textFont != null) textFont.dispose();
+        if (backgroundImage != null) backgroundImage.dispose();
     }
 }
